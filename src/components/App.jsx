9e58feaafaf2d5a0d114bbcdeeb8f4e74fd808cd
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-15 11:29:29 -0600

    Implemented video section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar.jsx";
 
 import Video from "../../assets/video.mp4";
 import Reflection from "../../assets/reflection.webp";
+import VideoCover from "../../assets/video-cover.webp";
 
 function App() {
   return (
@@ -28,10 +29,28 @@ function App() {
               Join the EarthFund community and help us decentralize the way we
               tackle humanity’s biggest problems.
             </p>
-            <button className="button">Request early access</button>
+            <button className="button container ">Request early access</button>
           </div>
         </div>
       </header>
+      <section className="voice-section container">
+        <h2 className="heading-2">
+          Use your voice to make&nbsp;the world a better place.
+        </h2>
+        <div className="voice-section__video">
+          <button className="button button--light button--video voice-section__button">
+            <svg width="24" height="24" viewBox="0 0 24 24" class="">
+              <path d="M17.604 9.414c1.972 1.16 1.972 4.012 0 5.172l-8.083 4.755C7.521 20.517 5 19.075 5 16.755V7.245c0-2.32 2.521-3.762 4.521-2.586l8.083 4.755z"></path>
+            </svg>
+            Watch our promo video
+          </button>
+          <img
+            className="voice-section__cover"
+            src={VideoCover}
+            alt="Cover for the video."
+          />
+        </div>
+      </section>
     </>
   );
 }

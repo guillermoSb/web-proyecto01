5a20c6280175330a7717ea001af08803896e960b
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-23 16:55:43 -0600

    Implemented deepak section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,8 @@ import Dif4 from "../../assets/dif-4.webp";
 import Dif5 from "../../assets/dif-5.webp";
 import Dif6 from "../../assets/dif-6.webp";
 
+import Deepak from "../../assets/deepak.webp";
+
 function App() {
   return (
     <>
@@ -124,6 +126,34 @@ function App() {
             className="difference-section__picture difference-section__picture--6"
             alt="App picture"
           />
+        </div>
+      </section>
+      <section className="community-section">
+        <h2 className="heading-2">
+          “A community where you&nbsp;have a real, meaningful voice”
+        </h2>
+        <div className="community-section__deepak flex">
+          <div className="flex align-center ">
+            <img
+              src={Deepak}
+              alt="Deepak Chopra"
+              className="community-section__deepak-picture"
+            />
+            <div className="community-section__deepak-info">
+              <h3 className="heading-3">
+                “I believe that each of us is a unique strand in the intricate
+                web of life and here to make a contribution. That's why we chose
+                to collaborate with EarthFund, a platform that gives everyone
+                the ability to participate and have a voice in making a more
+                sustainable, peaceful, healthier and joyful world.”
+              </h3>
+              <h4 className="heading-4">Deepak Chopra</h4>
+              <p className="text text--grey text--small">
+                Best-selling author and founder of the Mental Health and
+                Wellbeing cause
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
